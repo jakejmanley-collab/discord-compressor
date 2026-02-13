5f9ac653df9d870904eb7cf92f88e254b4a1f005
jakejmanley-collab <jakejmanley@gmail.com>
2026-02-13 11:40:18 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: '/',
+        headers: [
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
