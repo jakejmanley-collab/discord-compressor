bc1dbe8b501a4245d2372a486d3dcbb6c4fdc7d9
jakejmanley-collab <jakejmanley@gmail.com>
2026-02-13 12:17:35 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   headers: async () => {
     return [
       {
-        source: '/',
+        source: '/(.*)', // This ensures headers apply to ALL pages
         headers: [
           { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
